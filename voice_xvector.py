bbde347c1bf505392d7e19464445d4ff66f09d23
templepmet <templepmet@gmail.com>
2021-01-17 16:48:15 +0900

    add voice

--- voice_xvector.py
+++ voice_xvector.py
@@ -14,37 +14,65 @@ torchaudio.set_audio_backend("sox_io")
 # https://commonvoice.mozilla.org/ja/datasets
 class SpeechDataset(Dataset):
     sample_rate = 16000
-    def __init__(self, train=True, transform=None, split_rate=0.8):
-        tsv = './CommonVoice/cv-corpus-5.1-2020-06-22/ja/validated.tsv'
+    elem_second = 1
+    def __init__(self, kind="train", transform=None, split_rate=0.8):
+        # tsv = './CommonVoice/cv-corpus-5.1-2020-06-22/ja/validated.tsv' 
         # データセットの一意性確認と正解ラベルの列挙
-        import pandas as pd
-        df = pd.read_table(tsv)
-        assert not df.path.duplicated().any()
-        self.classes = df.client_id.drop_duplicates().tolist()
-        self.n_classes = len(self.classes)
+        # import pandas as pd
+        # df = pd.read_table(tsv)
+        # assert not df.path.duplicated().any()
+        # self.classes = df.client_id.drop_duplicates().tolist()
+        # self.n_classes = len(self.classes)
+
+        self.n_classes = 5
+        if kind == "unknown":
+            self.n_classes += 2
         
         # データセットの準備
         self.transform = transform
-        data_dirs = tsv.split('/')
-        dataset = torchaudio.datasets.COMMONVOICE(
-            '/'.join(data_dirs[:-4]), tsv=data_dirs[-1],
-            url='japanese', version=data_dirs[-3])
+        # data_dirs = tsv.split('/')
+        # dataset = torchaudio.datasets.COMMONVOICE(
+        #     '/'.join(data_dirs[:-4]), tsv=data_dirs[-1],
+        #     url='japanese', version=data_dirs[-3])
 
-        print(dataset[0][0].size())
+        dataset = []
+        for i in range(self.n_classes):
+            # 1つずつ読み込み
+            # waveform, sample_rate, labels に
+            waveform, sample_rate = torchaudio.load("voice/voice{}.mp3".format(i))
+            data = torch.split(waveform, sample_rate * self.elem_second, dim=1)
+            for wave in data:
+                if i >= 5:
+                    dataset.append([wave, sample_rate, -1])
+                else:
+                    dataset.append([wave, sample_rate, i])
 
         # データセットの分割
         n_train = int(len(dataset) * split_rate)
         n_val = len(dataset) - n_train
         torch.manual_seed(torch.initial_seed())  # 同じsplitを得るために必要
         train_dataset, val_dataset = random_split(dataset, [n_train, n_val])
-        self.dataset = train_dataset if train else val_dataset
+
+        if kind == "train":
+            self.dataset = train_dataset
+        elif kind == "val":
+            self.dataset = val_dataset
+        elif kind == "unknown":
+            self.dataset = val_dataset
+        else:
+            assert(False)
+
     def __len__(self):
         return len(self.dataset)
     def __getitem__(self, idx):
-        x, sample_rate, dictionary = self.dataset[idx]
+        x, sample_rate, label = self.dataset[idx]
         # リサンプリングしておくと以降は共通sample_rateでtransformできる
         if sample_rate != self.sample_rate:
             x = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=self.sample_rate)(x)
+        
+        # stereo to mono
+        x = torch.mean(x, dim=0).unsqueeze(0)
+
         # 各種変換、MFCC等は外部でtransformとして記述する
         # ただし、推論とあわせるためにMFCCは先にすませておく
         x = torchaudio.transforms.MFCC(log_mels=True)(x)
@@ -52,7 +80,7 @@ class SpeechDataset(Dataset):
         if self.transform:
             x = self.transform(x)
         # 特徴量:音声テンソル、正解ラベル:話者IDのインデックス
-        return x, self.classes.index(dictionary['client_id'])
+        return x, label
 
 # 学習モデル
 class SpeechNet(nn.Module):
@@ -155,6 +183,11 @@ def SpeechML(train_dataset=None, val_test_dataset=None, *,
         val_test_dataset.transform = test_transform
         val_test_dataloader = DataLoader(
             val_test_dataset, batch_size=batch_size)
+    if val_test_dataset:
+        unknown_dataset = SpeechDataset(kind="unknown")
+        unknown_dataset.transform = test_transform
+        unknown_dataloader = DataLoader(
+            unknown_dataset, batch_size=batch_size, shuffle=True)
     # 学習
     losses = []
     accs = []
@@ -177,6 +210,9 @@ def SpeechML(train_dataset=None, val_test_dataset=None, *,
         running_acc /= len(train_dataloader)
         losses.append(running_loss)
         accs.append(running_acc)
+
+
+
         # 検証ループ
         val_running_loss = 0.0
         val_running_acc = 0.0
@@ -191,16 +227,22 @@ def SpeechML(train_dataset=None, val_test_dataset=None, *,
             val_running_acc += torch.mean(pred.eq(y_val).float())
         val_running_loss /= len(val_test_dataloader)
         val_running_acc /= len(val_test_dataloader)
-        can_save = (val_running_acc > 0.9 and
-                    val_running_loss < min(val_losses))
+        # can_save = (val_running_acc > 0.9 and
+        #             (len(val_losses) == 0 or val_running_loss < min(val_losses)))
+        can_save = False
         val_losses.append(val_running_loss)
         val_accs.append(val_running_acc)
+        
+        
+        
         if show_progress:
             print(f'epoch:{epoch}, loss:{running_loss:.3f}, '
                   f'acc:{running_acc:.3f}, val_loss:{val_running_loss:.3f}, '
                   f'val_acc:{val_running_acc:.3f}, can_save:{can_save}')
         if save_state and can_save:   # あらかじめmodelフォルダを作っておくこと
             torch.save(model.state_dict(), f'model/0001-epoch{epoch:02}.pth')
+
+
     # グラフ描画
     if n_epochs > 0 and show_chart:
         fig, ax = plt.subplots(2)
@@ -223,37 +265,54 @@ def SpeechML(train_dataset=None, val_test_dataset=None, *,
     if test_last_hidden_layer:
         model.fc = model.fc[:-1]  # 最後の隠れ層を出力する
 
+    cos_th = 0.5
+
     y_preds = torch.Tensor()
-    for val_test in val_test_dataloader:
+    all_nums = 0
+    cnt = 0
+    for val_test in unknown_dataloader:
         x_test = val_test[0] if type(val_test) is list else val_test
         y_pred = model.eval()(x_test)
+
+        label = val_test[1].detach().numpy().copy()
+        # print(label)
+        # y = y_pred.detach().numpy().copy()
+        # print(y)
+
         if not test_last_hidden_layer:
             y_pred = torch.argmax(y_pred, dim=1)
-        y_preds = torch.cat([y_preds, y_pred])
+            # y_pred_val = torch.max(y_pred, dim=1)
+            # print(y_pred_val)
 
-        # x_vector_val = y_pred.detach().numpy().copy()
-        # preds = []
-        # for x_vector in x_vector_val:
-        #     maxcos = -2
-        #     pred = -1
-        #     for i in range(len(x_vector_speekers)):
-        #         cos = cos_sim(x_vector, x_vector_speekers[i])
-        #         if maxcos < cos:
-        #             maxcos = cos
-        #             pred = i
-        #     preds.append(pred)
+        x_vector_val = y_pred.detach().numpy().copy()
+        preds = []
+        preds_cos = []
+        for x_vector in x_vector_val:
+            maxcos = -2
+            pred = -1
+            for i in range(len(x_vector_speekers)):
+                cos = cos_sim(x_vector, x_vector_speekers[i])
+                if maxcos < cos:
+                    maxcos = cos
+                    pred = i
+            if maxcos < cos_th:
+                pred = -1
+            preds.append(pred)
+            preds_cos.append(maxcos)
         
-        # label = val_test[1].detach().numpy().copy()
-        # cnt = 0
-        # nums = len(label)
-        # for i in range(nums):
-        #     if label[i] == preds[i]:
-        #         cnt += 1
-        # print(cnt / nums)
+        nums = len(label)
+        for i in range(nums):
+            if label[i] == preds[i]:
+                cnt += 1
+        all_nums += nums
 
+        print(label)
+        print(preds)
+        print(preds_cos)
 
-        # print(val_test[1]) # 正解ラベル
-        # print(y_pred)
+    acc = cnt / all_nums
+    print(acc)
+
     return y_preds.detach()
 
 def cos_sim(v1, v2):
@@ -261,8 +320,8 @@ def cos_sim(v1, v2):
 
 # 呼び出しサンプル
 if __name__ == '__main__':
-    train_dataset = SpeechDataset(train=True)
-    val_dataset = SpeechDataset(train=False)
-    result = SpeechML(train_dataset, val_dataset, n_epochs=1,
+    train_dataset = SpeechDataset(kind="train")
+    val_dataset = SpeechDataset(kind="val")
+    result = SpeechML(train_dataset, val_dataset, n_epochs=3,
         show_chart=True, save_state=True, test_last_hidden_layer=True)
-    print(result)
+    # print(result)
